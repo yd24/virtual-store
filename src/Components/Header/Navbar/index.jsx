00001e297a2458edb4585886fdf0e1f49e4c2c7f
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-05-23 21:22:07 -0700

    Refactor code to make separate Cart component

--- src/Components/Header/Navbar/index.jsx
+++ src/Components/Header/Navbar/index.jsx
@@ -4,27 +4,17 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
 import MenuIcon from '@mui/icons-material/Menu';
 import Categories from '../../Categories';
+import Cart from '../Cart';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setActiveCategory } from '../../../Store/categorySlice';
-import { removeItem } from '../../../Store/cartSlice';
-import { incrementProduct } from '../../../Store/productSlice';
 
 export default function ButtonAppBar() {
   const [show, toggleShow] = React.useState(false);
-  const [cartAnchor, setCartAnchor] = React.useState(null);
-  const open = Boolean(cartAnchor);
-
   const dispatch = useDispatch();
-  const cartItems = useSelector(state => state.cart.cartItems);
-  const cartSize = useSelector(state => state.cart.totalCount);
   
   const resetProducts = () => {
     dispatch(setActiveCategory(null));
@@ -33,19 +23,6 @@ export default function ButtonAppBar() {
   const handleToggle = () => {
     toggleShow(!show);
   };
-
-  const handleCartClick = (e) => {
-    setCartAnchor(e.currentTarget);
-  }
-
-  const handleCartClose = () => {
-    setCartAnchor(null);
-  }
-
-  const removeFromCart = (item) => {
-    dispatch(removeItem(item.id));
-    dispatch(incrementProduct(item));
-  }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -66,39 +43,7 @@ export default function ButtonAppBar() {
               OUR STORE
             </Link>
           </Typography>
-          <Button 
-            id='cart-button'
-            color="inherit"
-            aria-controls={open ? 'cart-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleCartClick}
-          >
-            Cart ({cartSize})
-          </Button>
-          <Menu 
-            id='cart-menu'
-            anchorEl={cartAnchor}
-            open={open}
-            onClose={handleCartClose}
-            MenuListProps={{
-              'aria-labelledby': 'cart-button',
-            }}
-          >
-            {cartItems.length > 0
-            ?
-            cartItems.map((item, idx) => 
-              <MenuItem key={idx} sx={{display: 'flex', justifyContent: 'space-between', width: '300px', padding: '2em'}}>
-                {`${item.name} ${item.count > 1 ? `(${item.count})` : ``}`}
-                <span onClick={() => removeFromCart(item)}>X</span>
-              </MenuItem>
-            )
-            :
-            <MenuItem sx={{width: '300px', padding: '2em'}}>
-              Cart is Empty.
-            </MenuItem>
-            }
-          </Menu>
+          <Cart />
         </Toolbar>
       </AppBar>
       <Categories show={show} />
